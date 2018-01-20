a3dd600b2510f6714f7d44995960253d506f275b
Yu Sun <yu.sun.cs@gmail.com>
2018-01-20 08:49:05 -0800

    mario example

---
+++ MarioSatMorning/Assets/CoinScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Coin Trigger Enter! Coin"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (new Vector3(2, 0, 0));
+	}
+}
---
+++ MarioSatMorning/Assets/JumpSoundScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JumpSoundScript : MonoBehaviour {
+
+	public Text text;
+	public Text gameoverText;
+
+	public AudioSource jumpAudioSource;
+	public AudioSource coinAudioSource;
+
+	int counter;
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Coin Trigger Enter! Player"); 
+		if (other.gameObject.CompareTag ("Coin")) {
+			coinAudioSource.Play ();
+			Destroy (other.gameObject);
+			counter++;
+			text.text = "Coins: " + counter;
+		} 
+		if (other.gameObject.CompareTag ("Floor")) {
+			Destroy (gameObject);
+			gameoverText.enabled = true;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		gameoverText.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			jumpAudioSource.Play ();
+		}
+	}
+}
